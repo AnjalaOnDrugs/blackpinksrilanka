205889511c2c9937bb422fe313b126595776a821
Anjala <harisambolaya@gmail.com>
2026-01-24 00:25:02 +0530

    first commit

---
+++ js/support.js
@@ -0,0 +1,30 @@
+window.addEventListener("load", (event) => {
+
+    function resize() {
+        //document.getElementById("bp_logo").style.height = "30px"
+        var heights = document.getElementById("bp_logo").clientHeight
+        document.getElementById("img_gap").style.height = heights + 20 + "px";
+
+        
+    }
+    resize();
+    window.onresize = function () {
+        resize();
+    };
+
+    var lastScrollTop = 0;
+    var scroll_value = 100;
+
+
+window.addEventListener("scroll", function(){ 
+   var st = window.pageYOffset || document.documentElement.scrollTop; 
+   if (st > lastScrollTop) {
+    
+    document.getElementById("bp_logo").style.transform = "translate(0,"+Math.trunc(window.pageYOffset/5)+"px)"
+   } else if (st < lastScrollTop) {
+    
+    document.getElementById("bp_logo").style.transform = "translate(0,"+Math.trunc(window.pageYOffset/5)+"px)"
+   } 
+   lastScrollTop = st <= 0 ? 0 : st; 
+}, false);
+})
